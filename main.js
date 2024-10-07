fe5f3a26af7a8237636dca09c1ed1e3b35f42521
MisterBrash <matthew.brash@ocsb.ca>
2024-10-07 15:57:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /**
- * 2.2 - Variables and Math
+ * 2.3 - Mathematics
  * ICS3 - Mr. Brash 🐿️
  * 
  * Make sure to read the README.md (especially if you missed class)
@@ -8,8 +8,14 @@
  * Don't forget - these are playgrounds. Once you finish the requested task, feel free to play.
  **/ 
 
+// Get a random number between 0 and 100
+let random_number = Math.random() * 100
 
-// Some example variables
-let it_is_christmas = false
-let teaching_partner = "Mr. Squirrel"
-let number_of_students = 27
+// Round it to the nearest whole number
+random_number = Math.round(random_number)
+
+// Output the result usijng a new way to insert a variable into a string (called a template)
+console.log(`I generated the number ${random_number}`)
+
+// ***  Your code goes below  ***
+
